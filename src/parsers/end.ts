02d0b11b86a21998ab97d7dfc5acf55db0ddd71a
Gabriel Pinheiro <gabrielctpinheiro@gmail.com>
2021-09-12 14:36:59 -0300

    Added end of input alias

--- src/parsers/end.ts
+++ src/parsers/end.ts
@@ -2,11 +2,11 @@ import { Parser } from "../parser";
 import { Result } from "../result";
 
 export function endParser(): Parser<null> {
-    return new Parser(`EOF`, state => {
+    return new Parser('end of input', function (state) {
         if(state.code.length === state.index) {
             return Result.ok(state, null);
         }
 
-        return Result.fail(state, `Expected end of input, got "${state.code.slice(state.index, state.index + 16)}"`);
+        return Result.fail(state, `Expected ${this.name}, got "${state.code.slice(state.index, state.index + 16)}"`);
     });
 }
